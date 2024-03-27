e7fdec8b70b3c50198c434db7d05d58a4b5b5014
Jackie <44011334mm@stu.gdou.edu.cn>
2024-03-28 00:50:39 +0800

    feat: UUID生成器

--- src/utils/string.js
+++ src/utils/string.js
@@ -7,3 +7,14 @@
 export function replaceAt(originalString, index, replacement) {
   return originalString.substring(0, index) + replacement + originalString.substring(index + 1);
 }
+
+/**
+ * @returns {string} UUID
+ */
+export function UUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0,
+      v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
