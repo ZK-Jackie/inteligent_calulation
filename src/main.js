2267cdde6283a173be19e176a32fb695d5db7b7b
Jackie <44011334mm@stu.gdou.edu.cn>
2024-04-04 16:36:06 +0800

    perf: 优化观感结构

--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import $ from 'jquery'  //导入jquery
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// Global modules
+import $ from 'jquery'  //jquery
 import * as echarts from 'echarts'
-
-Vue.use(ElementUI);
+import ElementUI from 'element-ui';
+// Global components
+import Block from './components/block.vue'
 
 Vue.prototype.$echarts = echarts
 Vue.prototype.$ = $   //将$(jquery)挂在到vue.prototype身上，让所有的Vue共享这个方法
-Vue.config.productionTip = false
 
+Vue.use(ElementUI);
+
+Vue.component('Block', Block)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
