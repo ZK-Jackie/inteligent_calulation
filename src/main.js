cb2df6904a059d0922a6afb616401e19dd410580
Jackie <44011334mm@stu.gdou.edu.cn>
2024-04-06 01:06:10 +0800

    feat: 引入element ui

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import $ from 'jquery'  //jquery
 import * as echarts from 'echarts'
 import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // Global components
 import Block from './components/block.vue'
 
@@ -19,6 +20,7 @@ Vue.component('Block', Block)
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
