e3a55d3e901b1f7df3a2763c4a3bf5958fde456b
Jackie <44011334mm@stu.gdou.edu.cn>
2024-04-06 17:11:44 +0800

    feat: 添加过渡动画

---
+++ src/components/charts/loading.vue
@@ -0,0 +1,70 @@
+<script>
+import {UUID} from "@/utils/string";
+
+export default {
+  name: 'LoadingChart',
+  props: {
+    id: {
+      type: String,
+      required: false,
+      default: UUID()
+    }
+  },
+  mounted() {
+    const chart = this.$echarts.init(document.querySelector('.loading-' + this.id));
+    const option = {
+      graphic: {
+        elements: [
+          {
+            type: 'text',
+            left: 'center',
+            top: 'center',
+            style: {
+              text: '智算古稀',
+              fontSize: 80,
+              fontWeight: 'bold',
+              lineDash: [0, 200],
+              lineDashOffset: 0,
+              fill: 'transparent',
+              stroke: '#cdcdcd',
+              lineWidth: 1
+            },
+            keyframeAnimation: {
+              duration: 2500,
+              loop: true,
+              keyframes: [
+                {
+                  percent: 0.7,
+                  style: {
+                    fill: 'transparent',
+                    lineDashOffset: 200,
+                    lineDash: [200, 0]
+                  }
+                },
+                {
+                  // Stop for a while.
+                  percent: 0.8,
+                  style: {
+                    fill: 'transparent'
+                  }
+                },
+                {
+                  percent: 1,
+                  style: {
+                    fill: 'black'
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+    chart && chart.setOption(option);
+  }
+}
+</script>
+
+<template>
+  <div :class="'loading-'+ id" />
+</template>
