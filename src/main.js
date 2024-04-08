3209362d524548d98165bb08e84d451c9045e849
Jackie <44011334mm@stu.gdou.edu.cn>
2024-04-09 01:04:30 +0800

    bugfix: element ui css显示

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import $ from 'jquery'  //jquery
 import * as echarts from 'echarts'
 import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // Global components
 import Block from './components/block.vue'
 
