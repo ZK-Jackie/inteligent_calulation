1aedaafffc9b9c5641203af14db5f657c354717e
Jackie <44011334mm@stu.gdou.edu.cn>
2024-04-09 01:04:46 +0800

    feat: 知识图谱展示

--- src/views/PoliciesInfo/index.vue
+++ src/views/PoliciesInfo/index.vue
@@ -2,11 +2,11 @@
   <div class="main-box">
     <ul class="clearfix">
       <li>
-        <div class="all-box" style="height: 9.6rem">
-　　       <div class="all-title">模块标题样式</div>
-          <div class="allnav" id="echart1" style="width: 100%; height: 100%"></div>
-          <div class="all-box-foot"></div>
-        </div>
+        <Block title="养老政策知识图谱">
+          <PinchZoom style="background-color: #ffffff; height: 8.7rem">
+            <img src="@/assets/policyGraph.svg" alt="养老保险政策知识图谱"/>
+          </PinchZoom>
+        </Block>
       </li>
 
       <li>
@@ -34,7 +34,7 @@
 </template>
 
 <script>
-import * as echarts from "echarts";
+import PinchZoom from "vue-pinch-zoom";
 import PopularDocument from "@/views/PoliciesInfo/components/PopularDocument.vue";
 import Block from "@/components/block.vue";
 import WorldCloud  from "@/views/PoliciesInfo/components/WorldCloud.vue";
@@ -42,7 +42,7 @@ import SearchBox  from "@/views/PoliciesInfo/components/SearchBox.vue";
 
 export default {
   components:{
-    PopularDocument,Block,WorldCloud,SearchBox
+    PopularDocument,Block,WorldCloud,SearchBox,PinchZoom
   },
   data(){
     return{
@@ -51,113 +51,112 @@ export default {
     }
   },
   methods: {
-    echarts_1() {
-      var that = this;
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById('echart1'));
-
-      var option = {
-        //  backgroundColor: '#00265f',
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'shadow'
-          }
-        },
-        grid: {
-          left: '0%',
-          top: '10px',
-          right: '0%',
-          bottom: '4%',
-          containLabel: true
-        },
-        xAxis: [{
-          type: 'category',
-          data: ['商超门店', '教育培训', '房地产', '生活服务', '汽车销售', '旅游酒店', '五金建材'],
-          axisLine: {
-            show: true,
-            lineStyle: {
-              color: "rgba(255,255,255,.1)",
-              width: 1,
-              type: "solid"
-            },
-          },
-
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            interval: 0,
-            // rotate:50,
-            show: true,
-            splitNumber: 15,
-            textStyle: {
-              color: "rgba(255,255,255,.6)",
-              fontSize: '12',
-            },
-          },
-        }],
-        yAxis: [{
-          type: 'value',
-          axisLabel: {
-            //formatter: '{value} %'
-            show: true,
-            textStyle: {
-              color: "rgba(255,255,255,.6)",
-              fontSize: '12',
-            },
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLine: {
-            show: true,
-            lineStyle: {
-              color: "rgba(255,255,255,.1	)",
-              width: 1,
-              type: "solid"
-            },
-          },
-          splitLine: {
-            lineStyle: {
-              color: "rgba(255,255,255,.1)",
-            }
-          }
-        }],
-        series: [
-          {
-            type: 'bar',
-            data: [200, 300, 300, 900, 1500, 1200, 600],
-            barWidth: '35%', //柱子宽度
-            // barGap: 1, //柱子之间间距
-            itemStyle: {
-              normal: {
-                color: '#2f89cf',
-                opacity: 1,
-                barBorderRadius: 5,
-              }
-            }
-          }
-
-        ]
-      };
-
-      // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option);
-      window.addEventListener("resize", function () {
-        myChart.resize();
-      });
-    },
-    load(){
-      // 延迟初始化图表，直到可以确定DOM元素已经有了正确的大小
-      setTimeout(()=>{
-        this.echarts_1()
-      },2000)
-    },
-
+    // echarts_1() {
+    //   var that = this;
+    //   // 基于准备好的dom，初始化echarts实例
+    //   var myChart = echarts.init(document.getElementById('echart1'));
+    //
+    //   var option = {
+    //     //  backgroundColor: '#00265f',
+    //     tooltip: {
+    //       trigger: 'axis',
+    //       axisPointer: {
+    //         type: 'shadow'
+    //       }
+    //     },
+    //     grid: {
+    //       left: '0%',
+    //       top: '10px',
+    //       right: '0%',
+    //       bottom: '4%',
+    //       containLabel: true
+    //     },
+    //     xAxis: [{
+    //       type: 'category',
+    //       data: ['商超门店', '教育培训', '房地产', '生活服务', '汽车销售', '旅游酒店', '五金建材'],
+    //       axisLine: {
+    //         show: true,
+    //         lineStyle: {
+    //           color: "rgba(255,255,255,.1)",
+    //           width: 1,
+    //           type: "solid"
+    //         },
+    //       },
+    //
+    //       axisTick: {
+    //         show: false,
+    //       },
+    //       axisLabel: {
+    //         interval: 0,
+    //         // rotate:50,
+    //         show: true,
+    //         splitNumber: 15,
+    //         textStyle: {
+    //           color: "rgba(255,255,255,.6)",
+    //           fontSize: '12',
+    //         },
+    //       },
+    //     }],
+    //     yAxis: [{
+    //       type: 'value',
+    //       axisLabel: {
+    //         //formatter: '{value} %'
+    //         show: true,
+    //         textStyle: {
+    //           color: "rgba(255,255,255,.6)",
+    //           fontSize: '12',
+    //         },
+    //       },
+    //       axisTick: {
+    //         show: false,
+    //       },
+    //       axisLine: {
+    //         show: true,
+    //         lineStyle: {
+    //           color: "rgba(255,255,255,.1	)",
+    //           width: 1,
+    //           type: "solid"
+    //         },
+    //       },
+    //       splitLine: {
+    //         lineStyle: {
+    //           color: "rgba(255,255,255,.1)",
+    //         }
+    //       }
+    //     }],
+    //     series: [
+    //       {
+    //         type: 'bar',
+    //         data: [200, 300, 300, 900, 1500, 1200, 600],
+    //         barWidth: '35%', //柱子宽度
+    //         // barGap: 1, //柱子之间间距
+    //         itemStyle: {
+    //           normal: {
+    //             color: '#2f89cf',
+    //             opacity: 1,
+    //             barBorderRadius: 5,
+    //           }
+    //         }
+    //       }
+    //
+    //     ]
+    //   };
+    //
+    //   // 使用刚指定的配置项和数据显示图表。
+    //   myChart.setOption(option);
+    //   window.addEventListener("resize", function () {
+    //     myChart.resize();
+    //   });
+    // },
+    // load(){
+    //   // 延迟初始化图表，直到可以确定DOM元素已经有了正确的大小
+    //   setTimeout(()=>{
+    //     this.echarts_1()
+    //   },2000)
+    // },
   },
   mounted() {
-    this.load();
+    // this.load();
   }
 }
 </script>
