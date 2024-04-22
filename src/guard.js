3771e506a72fb6c859c3699a9d626349f352c831
gdsbapple <13790793568@stu.gdou.edu.cn>
2024-04-22 23:31:14 +0800

    bugfix：解决了过场动画路由冗杂而形成的bug

--- src/guard.js
+++ src/guard.js
@@ -6,68 +6,31 @@ import 'nprogress/nprogress.css'
 NProgress.configure({showSpinner: false})
 
 let timer;//定时器变量
+let list = ['/nation', '/regin', '/policy', '/modify'];
+let n = 0;
 
 router.beforeEach((to, from, next) => {
   NProgress.start();
   if(from.fullPath.includes("modify") && to.fullPath.includes("regin")){
     next('gd');
-    NProgress.done()
     return;
-  }else{
-    next();
     NProgress.done()
+  }else if(from.fullPath === '/home' && list.includes(to.path) && n === 0) {
+
+    next('/animation');
+
+    // 设置定时器，2秒后跳转
+    timer = setTimeout(() => {
+      router.push(to.fullPath);
+    }, 2000);
     return;
-  }
-});
-
-//首界面跳转到任一界面的过场动画配置
-router.beforeEach((to, from, next) => {
-  NProgress.start();
-
-  clearTimeout(timer);
-
-  if (from.fullPath === '/' && to.fullPath === '/nation') {
-
-    next({ path: '/animation' });
-
-    // 设置定时器，3秒后跳转
-    timer = setTimeout(() => {
-      router.push('/nation');
-    }, 3000);
-  } else  if (from.fullPath === '/' && to.fullPath === '/regin') {
-
-    next({ path: '/animation' });
-
-    // 设置定时器，5秒后跳转
-    timer = setTimeout(() => {
-      router.push('/regin');
-    }, 3000);
-  } else  if (from.fullPath === '/' && to.fullPath === '/policy') {
-
-    next({ path: '/animation' });
-
-    // 设置定时器，5秒后跳转
-    timer = setTimeout(() => {
-      router.push('/policy');
-    }, 3000);
-  } else  if (from.fullPath === '/' && to.fullPath === '/modify') {
-
-    next({ path: '/animation' });
-
-    // 设置定时器，5秒后跳转
-    timer = setTimeout(() => {
-      router.push('/modify');
-    }, 3000);
   } else {
     next();
+    return;
+    NProgress.done()
   }
-
-  NProgress.done();
 });
 
-router.afterEach(() => {
-  NProgress.done()
-})
 
 router.afterEach(() => {
   NProgress.done()
